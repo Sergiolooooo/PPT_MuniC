def90c2a65992bbc6d18beb875815dd8793b6425
Sergiolooooo <selopezmo@est.utn.ac.cr>
2025-03-04 13:01:06 -0600

    quitar longitud, latitud y colocar url Google

--- src/models/comercios.js
+++ src/models/comercios.js
@@ -17,10 +17,10 @@ const getDatosByCategoria = async (categoria) => {
 };
 
 const CreateDatos = async (datos) => {
-    const { nombre_comercio, descripcion_comercio, latitud, longitud, telefono, video_youtube, id_categoria } = datos;
+    const { nombre_comercio, descripcion_comercio, url_google, telefono, video_youtube, id_categoria } = datos;
     const [result] = await database.query(
-        'CALL Sp_CreateComercio(?,?,?,?,?,?,?)',
-        [nombre_comercio, descripcion_comercio, latitud, longitud, telefono, video_youtube, id_categoria]
+        'CALL Sp_CreateComercio(?,?,?,?,?,?)',
+        [nombre_comercio, descripcion_comercio, url_google, telefono, video_youtube, id_categoria]
     );
 
     return result;  // Devuelve el resultado de la consulta que contiene affectedRows
