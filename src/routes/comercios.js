8d14806cf6a75a07e639e6be78b4ba132909a368
keytc234 <161094502+keytc234@users.noreply.github.com>
2025-04-01 23:03:18 -0600

    Cambios realizados por Keylor

--- src/routes/comercios.js
+++ src/routes/comercios.js
@@ -14,11 +14,11 @@ const router = Router();
 
 //router.get('/',validateToken ,getMethod); ASI SE USA EL VALIDATE TOKEN
 
-router.get('/', getMethod);
-router.get('/categoria', getComerciosByCategoria);
-router.get('/:id', getMethodById);
-router.post('/', postMethod);
-router.put('/:id', updateMethod);
-router.delete('/:id', deleteMethod);
+router.get('/',validateToken, getMethod);
+router.get('/categoria',validateToken, getComerciosByCategoria);
+router.get('/:id',validateToken, getMethodById);
+router.post('/',validateToken, postMethod);
+router.put('/:id',validateToken, updateMethod);
+router.delete('/:id',validateToken, deleteMethod);
 
 module.exports = router;
