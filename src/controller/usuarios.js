234ac650e073dfe997747c50cf585433f1f6ded3
keytc234 <161094502+keytc234@users.noreply.github.com>
2025-04-01 21:53:06 -0600

    cambio

--- src/controller/usuarios.js
+++ src/controller/usuarios.js
@@ -147,7 +147,7 @@ const methodLogin = async (req, res) => {
             maxAge: 3600000, // 1 hora
             sameSite: 'None',
             path: '/',
-            domain: '.ppt-munic.onrender.com'
+            domain: 'practicafrontppt-production.up.railway.app'
         });
 
         res.json({ message: 'Inicio Exitoso.', user: usuarioConRoles });
